c3060631a69a09009a4a6474b035750a6064073f
Mike DiDomizio <mikedidomizio@gmail.com>
2024-03-06 22:08:13 -0500

    detect-secrets: separate file to test against

---
+++ detect-secrets.js
@@ -0,0 +1,4 @@
+// Purpose of this separate file is detect-secrets won't flag a key with the secret for some reason?
+
+// The following is for demo purposes and not a secret
+export const KEY="AKIAZZZTHISISFAKEZZZ"
